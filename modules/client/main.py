b05a81550fd216270e591d336922fd5c7f16e78f
hikaru-n <hikaru_nozoe@albert2005.co.jp>
2021-06-22 08:24:48 +0900

    wip

--- modules/client/main.py
+++ modules/client/main.py
@@ -5,7 +5,7 @@ from threading import Thread
 from queue import Queue
 
 import cv2
-from azure.iot.device import IoTHubDeviceClient
+from azure.iot.device import IoTHubDeviceClient, Message
 
 
 class IoTHub(Thread):
@@ -17,27 +17,24 @@ class IoTHub(Thread):
     def _get_device_client(self):
         con_str = os.environ.get("CONNECTION_STRING", "")
         if not con_str:
-            raise ValueError("Environ name CONNECTION_STRING must be set.")
+            raise RuntimeError("Environ name CONNECTION_STRING must be set.")
         return IoTHubDeviceClient.create_from_connection_string(con_str)
 
     def _check_message_include_estimater_results(self, message):
         if not isinstance(message, Dict):
             raise TypeError
 
-        if not message.get("Prediction", ""):
+        if message.get("Prediction", None) is None:
             raise ValueError
 
-        if not message.get("ElapsedTime", ""):
+        if message.get("ElapsedTime", None) is None:
             raise ValueError
-
-    def _format_message(self, message):
-        self._check_message_include_estimater_results(message)
-        return "{}"
 
     def _send(self):
-        if self.messages.not_empty():
-            message = self.messages.get()
-            message = self._format_message(message)
+        if not self.messages.empty():
+            data = self.messages.get()
+            self._check_message_include_estimater_results(data)
+            message = Message(str(data))
             self._device_client.send_message(message)
 
     def run(self):

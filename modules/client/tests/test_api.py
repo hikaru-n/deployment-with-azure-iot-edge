1fb1e0a5f76c0f7b9eef95d964bbffadd1c51c52
hikaru-n <hikaru.nozoe@gmail.com>
2021-11-06 15:47:47 +0900

    Separate test

--- modules/client/tests/test_api.py
+++ modules/client/tests/test_api.py
@@ -1,18 +1,31 @@
 import pytest
-from api import get_api_url
+from iotclient import get_api_url, get_azure_connection_string
 
 
-def replace_env(mocker, name, value=None):
+def _overwrite_environment_value(mocker, name, value=None):
     if value is None:
-        return
-    mocker.patch("os.environ", {name: value})
-
-
-@pytest.mark.parametrize("env_name, value", [("", None), ("API_HOST", "estimater")])
-def test_get_api_url(mocker, env_name, value):
-    replace_env(mocker, env_name, value)
-
-    if value is None:
-        assert get_api_url() == "http://localhost:5000"
+        mocker.patch("os.environ", {})
     else:
-        assert get_api_url() == f"http://{value}:80"
+        mocker.patch("os.environ", {name: value})
+
+
+def test_cannot_get_connection_string_if_unnset_environment_value(mocker):
+    _overwrite_environment_value(mocker, "AZURE_CONNECTION_STRING")
+    with pytest.raises(RuntimeError):
+        get_azure_connection_string()
+
+
+def test_can_get_connection_string(mocker):
+    _overwrite_environment_value(mocker, "AZURE_CONNECTION_STRING", "authorized")
+    get_azure_connection_string()
+
+
+def test_api_url_has_localhost_when_unset_api_host(mocker):
+    _overwrite_environment_value(mocker, "API_HOST")
+    assert get_api_url() == "http://localhost:5000"
+
+
+def test_api_url_has_specified_host_name(mocker):
+    hostname = "awesome"
+    _overwrite_environment_value(mocker, "API_HOST", hostname)
+    assert get_api_url() == f"http://{hostname}:80"

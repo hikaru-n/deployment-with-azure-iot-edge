dec25faa563fbb37890cdcfbbb480092e9aef1b5
hikaru-n <hikaru.nozoe@gmail.com>
2021-11-06 17:28:06 +0900

    Add simple API

--- modules/serving/src/iotserving/entrypoints/app.py
+++ modules/serving/src/iotserving/entrypoints/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
+from iotserving.models import get_model
+
 app = Flask(__name__)
 
 
-@app.route("/models/<str:name>")
+@app.route("/models/<string:modelname>/predict")
 def predict(modelname):
-    return "Hello {}!".format(modelname)
+    model = get_model(modelname)
+    return "Hello {}!".format(model)

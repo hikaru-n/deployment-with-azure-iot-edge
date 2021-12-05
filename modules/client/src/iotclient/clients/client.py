244369de2a0ee9eb20ebe4df7757668f88ea80ea
hikaru-n <hikaru.nozoe@gmail.com>
2021-12-05 16:43:03 +0900

    Add shutdown

--- modules/client/src/iotclient/clients/client.py
+++ modules/client/src/iotclient/clients/client.py
@@ -1,16 +1,23 @@
-from queue import Queue
+import queue
 
-from iotclient.clients import Camera, IoTHub
+from iotclient.clients import Camera, IoTHub, APIClient
 
 
 class Client:
     def __init__(self, buffersize=3):
-        self.images = Queue(buffersize)
-        self.camera = Camera(images=self.images)
+        images = queue.Queue(buffersize)
+        self.camera = Camera(images=images)
 
-        self.messages = Queue(buffersize)
-        self.iothub = IoTHub(messages=self.messages)
+        responses = queue.Queue(buffersize)
+        self.api = APIClient(images, responses)
+
+        self.iothub = IoTHub(responses)
 
     def run(self):
         self.camera.start()
+        self.api.start()
         self.iothub.start()
+
+    def shutdown(self):
+        self.camera.kill()
+        self.api.kill()

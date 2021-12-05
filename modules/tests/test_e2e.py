d88b580b711d94edf162fa7b195aec4041bf2bee
hikaru-n <hikaru.nozoe@gmail.com>
2021-12-05 19:37:18 +0900

    Add test case for e2e test

--- modules/tests/test_e2e.py
+++ modules/tests/test_e2e.py
@@ -17,8 +17,28 @@ def use_fake_camera_capture(mocker):
     mocker.patch("cv2.VideoCapture", _FakeVideoCapture)
 
 
+class _FakeIoTHubClient:
+    def __init__(self, responses):
+        self.responses = responses
+
+    def start(self):
+        pass
+
+    def kill(self):
+        pass
+
+
+@pytest.fixture
+def nullify_iothub(mocker):
+    mocker.patch("iotclient.clients.client.IoTHub", _FakeIoTHubClient)
+
+
+@pytest.mark.usefixtures("nullify_iothub")
 @pytest.mark.usefixtures("use_fake_camera_capture")
 def test():
     client = Client()
     client.run()
     client.shutdown()
+
+    actual = client.iothub.responses.get()
+    assert actual._value.get("Prediction") == 285

459c4322b27455d9cb6a80d9920c803bb469c803
hikaru-n <hikaru.nozoe@gmail.com>
2021-12-12 15:25:20 +0900

    Add test camera with mockup

---
+++ modules/client/tests/test_camera.py
@@ -0,0 +1,44 @@
+import os
+from queue import Queue
+
+import numpy as np
+from iotclient.clients import Camera
+import pytest
+from . import get_image
+
+
+def not_found_web_camera():
+    number = os.environ.get("INDEX", 0)
+    return not os.path.exists(f"/dev/index{number}")
+
+
+class _FakeVideoCapture:
+    def __init__(self, *args, **kwags):
+        pass
+
+    def read(self):
+        return get_image("cat.jpg")
+
+
+def use_fake_camera_capture(mocker):
+    mocker.patch("cv2.VideoCapture", _FakeVideoCapture)
+
+
+class TestCameraClient:
+    def _test_run(self, images):
+        client = Camera(images)
+        client._run()
+
+    @pytest.mark.skipif(not_found_web_camera(), reason="not found web camera")
+    def test_camera_capture(self):
+        images = Queue(1)
+        self._test_run(images)
+
+    def test_camera_capture_local_image(self, mocker):
+        use_fake_camera_capture(mocker)
+        images = Queue(1)
+        self._test_run(images)
+
+        actual = images.get()
+        expect = get_image("cat.jpg")
+        np.testing.assert_allclose(actual, expect)
